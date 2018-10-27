238c3a3e6ca96518efc9d6834520c6e5d51f06b9
Diego Lopes <djkakaroto@gmail.com>
2018-10-26 21:41:20 -0300

    Adicionado links de algoritmo trading em R e tutoriais

--- Econometria/Aula01.r
+++ Econometria/Aula01.r
@@ -1,2 +1,26 @@
 # Removendo objetos da memória
 rm( list = ls() )
+
+# Series temporais com forecast (ts)
+install.packages("forecast")
+
+# Usando a biblioteca forecast
+library(forecast)
+
+# Links para trading in R
+# https://www.datacamp.com/community/tutorials/r-trading-tutorial
+# https://www.datacamp.com/courses/model-a-quantitative-trading-strategy-in-r/
+# https://rpubs.com/lance4869/delta_hedging
+# https://rpubs.com/OmarMa/Equity_Portfolio_Hedging
+# https://www.quantinsti.com/blog/predictive-modeling-algorithmic-trading
+# https://github.com/Apress/automated-trading-with-r
+
+
+
+# Pacotes para trabalhar com trading no R (TTR)
+# install.packages("TTR")
+# Usando o pacote de trading
+# library(TTR)
+# JavaScript Charting Library
+# install.packages("dygraphs")
+
